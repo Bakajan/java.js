f7c86651fe196a1d7ac11f2be0cbc6a21224f1bd
Bakajan <omega3mini@yahoo.com>
2016-05-13 12:43:11 -0400

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -9,31 +9,74 @@ function Menu(label)
 		
 		con.innerHTML = this.label;
 		con.id = this.id;
-		con.style.position = "absolute";
+		//con.style.display = "none";
+		con.style.position = "relative";
+		con.style.display = "block";
 		con.style.zindex = 1;
 		con.style.cursor = "pointer";
-		console.log("add self to " + parent);
+		this.backgroundColor;
+		this.textColor;
 		
 		if(document.getElementById(parent) != null)
 		{
-			console.log("parent is here");
+			this.id = this.id + "_" + id;
+			con.id = this.id
+			document.getElementById(parent).appendChild(con);
+			
 			if(this.mouseOverListener != null)
 				con.addEventListener('mouseover', this.mouseOverListener, false);
+			else
+			{
+				con.addEventListener('mouseover', 
+				function() 
+				{ 
+					var el = document.getElementById(this.id);
+					console.log("On " + el.id);
+					if(el === event.target)
+					{
+						this.backgroundColor = "blue";
+						this.textColor = "white";
+						if(document.getElementById(this.id) != null)
+						{
+							document.getElementById(this.id).style.backgroundColor = this.backgroundColor; 
+							document.getElementById(this.id).style.color = this.textColor;
+						}
+					}
+				}, false);
+			}
 			if(this.mouseOutListener != null)
 				con.addEventListener('mouseout', this.mouseOutListener, false);
+			else
+			{
+				con.addEventListener('mouseout', 
+				function() 
+				{ 
+					var el = document.getElementById(this.id);
+					if(el === event.target)
+					{
+						this.backgroundColor = "";
+						this.textColor = "black";
+						if(document.getElementById(this.id) != null)
+						{
+							document.getElementById(this.id).style.backgroundColor = this.backgroundColor; 
+							document.getElementById(this.id).style.color = this.textColor;
+						} 
+					}
+				}, false);
+			}
+			
 			if(this.mouseClickListener != null)
 				con.addEventListener('click', this.mouseClickListener, false);
-				
-			this.id = this.id + "_" + id;
-			document.getElementById(parent).appendChild(con);
 		}
-
-		if(this.children.length > 0)
+		
+		/*
+		if(this.children > 0)
 		{
 			for(var i = 0; i!= this.children.length; i++)
 			{
 				this.children[i].addSelfTo(this.id, i);
 			}
 		}
+		*/
 	}
 }
